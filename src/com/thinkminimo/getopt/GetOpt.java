a3e3f1d4053c7cc394566f848edaf8f225aaef9b
Micha Niskin <asdf@ubergibson.com>
2009-02-15 17:32:42 -0500

    added features and removed the Main file

--- src/com/thinkminimo/getopt/GetOpt.java
+++ src/com/thinkminimo/getopt/GetOpt.java
@@ -6,14 +6,21 @@ import gnu.getopt.LongOpt;
 
 public class GetOpt {
 
-  private ArrayList<LongOpt>      mLongOpts   = new ArrayList<LongOpt>();
-  private ArrayList<String>       mDescs      = new ArrayList<String>();
-  private HashMap<String, String> mOpts       = new HashMap<String, String>();
+  private ArrayList<LongOpt>      mLongOpts     = new ArrayList<LongOpt>();
+  private ArrayList<String>       mDescs        = new ArrayList<String>();
 
-  private String                  mAppname    = "";
-  private String[]                mArgv       = null;
-  private File                    mFile       = null;
-  private ArrayList<String>       mNotOpts    = null;
+  private ArrayList<String>       mNotOpts      = new ArrayList<String>();
+  private ArrayList<String>       mNotOptDescs  = new ArrayList<String>();
+  private ArrayList<String>       mNotOptExtra  = new ArrayList<String>();
+
+  private HashMap<String, String> mOpts         = new HashMap<String, String>();
+
+  private String                  mAppname      = "";
+  private String[]                mArgvIn       = null;
+  private File                    mFile         = null;
+
+  private String                  mVarArg       = null;
+  private String                  mVarArgDesc   = null;
 
   public GetOpt(String[] argv) {
     this("getopt", argv);
@@ -21,9 +28,10 @@ public class GetOpt {
 
   public GetOpt(String name, String[] argv) {
     mAppname = name;
-    mArgv    = argv;
-    
-    addFlag("help", "Print this help info and exit.");
+    mArgvIn  = argv;
+
+    mLongOpts.add(new LongOpt("help", LongOpt.NO_ARGUMENT,  null, 4));
+    mDescs.add("Print this help info and exit.");
 
     mLongOpts.add(new LongOpt("showconfig", LongOpt.NO_ARGUMENT,  null, 3));
     mDescs.add("Print the saved configuration for this application and exit.");
@@ -33,6 +41,47 @@ public class GetOpt {
         save();
       }
     });
+  }
+
+  public GetOpt addOpt(String lng) {
+    return addOpt(lng, null);
+  }
+
+  public GetOpt addOpt(String lng, String desc) {
+    mLongOpts.add(
+        new LongOpt(lng, LongOpt.REQUIRED_ARGUMENT,  null, 1));
+    mDescs.add(desc == null ? "" : desc);
+    return this;
+  }
+
+  public GetOpt addFlag(String lng) {
+    return addFlag(lng, null);
+  }
+
+  public GetOpt addFlag(String lng, String desc) {
+    mLongOpts.add(new LongOpt(lng, LongOpt.NO_ARGUMENT,  null, 2));
+    mDescs.add(desc == null ? "" : desc);
+    return this;
+  }
+
+  public GetOpt addArg(String lng) {
+    return addArg(lng, null);
+  }
+
+  public GetOpt addArg(String lng, String desc) {
+    mNotOpts.add(lng);
+    mNotOptDescs.add(desc == null ? "" : desc);
+    return this;
+  }
+
+  public GetOpt addVarArg(String lng) {
+    return addVarArg(lng, null);
+  }
+
+  public GetOpt addVarArg(String lng, String desc) {
+    mVarArg     = lng;
+    mVarArgDesc = desc;
+    return this;
   }
 
   public GetOpt setFile(File file) {
@@ -70,15 +119,16 @@ public class GetOpt {
     } catch (Exception e) {}
   }
 
-  public GetOpt go() {
+  public GetOpt go() throws Exception {
     load();
     LongOpt[] l = mLongOpts.toArray(new LongOpt[mLongOpts.size()]);
-    gnu.getopt.Getopt g = new gnu.getopt.Getopt(mAppname, mArgv, "", l);
+    gnu.getopt.Getopt g = new gnu.getopt.Getopt(mAppname, mArgvIn, "", l);
     int c;
     while ((c = g.getopt()) != -1) {
       switch (c) {
         case '?':
-          System.exit(1);
+          printUsage();
+          throw new Exception();
         case 1:
           mOpts.put(mLongOpts.get(g.getLongind()).getName(), g.getOptarg());
           break;
@@ -88,6 +138,28 @@ public class GetOpt {
         case 3:
           showConfig();
           break;
+        case 4:
+          printUsage();
+          System.exit(1);
+      }
+    }
+    for (int i=g.getOptind(); i<mArgvIn.length; i++) {
+      String name;
+      if ((name = mNotOpts.get(i-g.getOptind())) != null) {
+        setOpt(mNotOpts.get(i-g.getOptind()), mArgvIn[i]);
+      } else {
+        if (mVarArg == null) {
+          System.err.println(mAppname+": unexpected argument: "+mArgvIn[i]);
+          printUsage();
+          throw new Exception();
+        }
+        mNotOptExtra.add(mArgvIn[i]);
+      }
+    }
+    for (int i=0; i<mNotOpts.size(); i++) {
+      if (getOpt(mNotOpts.get(i)) == null) {
+        System.err.println(mAppname+": missing argument: "+mNotOpts.get(i));
+        throw new Exception();
       }
     }
     return this;
@@ -102,24 +174,6 @@ public class GetOpt {
     return this;
   }
 
-  public ArrayList<String> getArgv() {
-    if (mNotOpts == null) {
-      mNotOpts = new ArrayList<String>();
-      LongOpt[] l = mLongOpts.toArray(new LongOpt[mLongOpts.size()]);
-      gnu.getopt.Getopt g = new gnu.getopt.Getopt(mAppname, mArgv, "", l);
-      int c;
-      while ((c = g.getopt()) != -1) {
-        switch (c) {
-          case '?':
-            System.exit(1);
-        }
-      }
-      for (int i=g.getOptind(); i<mArgv.length; i++)
-        mNotOpts.add(mArgv[i]);
-    }
-    return mNotOpts;
-  }
-
   public boolean getFlag(String opt) {
     return Boolean.valueOf(mOpts.get(opt));
   }
@@ -129,25 +183,8 @@ public class GetOpt {
     return this;
   }
 
-  public GetOpt addOpt(String lng) {
-    return addOpt(lng, null);
-  }
-
-  public GetOpt addOpt(String lng, String desc) {
-    mLongOpts.add(
-        new LongOpt(lng, LongOpt.REQUIRED_ARGUMENT,  null, 1));
-    mDescs.add(desc == null ? "" : desc);
-    return this;
-  }
-
-  public GetOpt addFlag(String lng) {
-    return addFlag(lng, null);
-  }
-
-  public GetOpt addFlag(String lng, String desc) {
-    mLongOpts.add(new LongOpt(lng, LongOpt.NO_ARGUMENT,  null, 2));
-    mDescs.add(desc == null ? "" : desc);
-    return this;
+  public ArrayList<String> getExtra() {
+    return mNotOptExtra;
   }
 
   public void showConfig() {
@@ -157,28 +194,67 @@ public class GetOpt {
     System.exit(0);
   }
 
-  public void printUsage(int status) {
-    System.out.println("OPTIONS:");
-    System.out.printf("\n");
+  public void printUsage() {
+    String usage = "Usage: java -jar "+mAppname+".jar";
+    
+    if (mLongOpts.size() > 0)
+      usage += " [OPTIONS]";
 
-    for (int i = 0; i < mLongOpts.size(); i++) {
-      LongOpt       o       = mLongOpts.get(i);
-      String        name    = o.getName();
-      int           flag    = o.getVal();
-      String        desc    = mDescs.get(i);
-      int           harg    = o.getHasArg();
+    if (mNotOpts.size() > 0)
+      for (String s : mNotOpts)
+        usage += " <"+s+">";
 
-      String arg = "";
-      if (harg == LongOpt.REQUIRED_ARGUMENT)
-        arg = " <arg>";
-      else if (harg == LongOpt.OPTIONAL_ARGUMENT)
-        arg = " [arg]";
+    if (mVarArg != null)
+      usage += " <"+mVarArg+"...>";
 
-      System.out.printf("    --%s%s\n", name, arg);
-      para(desc, 65, "        ");
-      System.out.printf("\n");
+    System.out.println(usage);
+
+    if (mNotOpts.size() > 0) {
+      System.out.print("\n");
+      System.out.println("ARGUMENTS:");
+      System.out.print("\n");
+
+      for (int i = 0; i < mNotOpts.size(); i++) {
+        String        name    = mNotOpts.get(i);
+        String        desc    = mNotOptDescs.get(i);
+
+        System.out.printf("    %s\n", name);
+        para(desc, 65, "        ");
+        System.out.printf("\n");
+      }
+
+      if (mVarArg != null) {
+        String        name    = mVarArg;
+        String        desc    = mVarArgDesc;
+
+        System.out.printf("    %s\n", name);
+        para(desc, 65, "        ");
+        System.out.printf("\n");
+      }
     }
-    System.exit(status);
+
+    if (mLongOpts.size() > 0) {
+      System.out.println("OPTIONS:");
+      System.out.print("\n");
+
+      for (int i = 0; i < mLongOpts.size(); i++) {
+        LongOpt       o       = mLongOpts.get(i);
+        String        name    = o.getName();
+        int           flag    = o.getVal();
+        String        desc    = mDescs.get(i);
+        int           harg    = o.getHasArg();
+
+        String arg = "";
+        if (harg == LongOpt.REQUIRED_ARGUMENT)
+          arg = " <arg>";
+        else if (harg == LongOpt.OPTIONAL_ARGUMENT)
+          arg = " [arg]";
+
+        System.out.printf("    --%s%s\n", name, arg);
+        para(desc, 65, "        ");
+        System.out.printf("\n");
+      }
+    }
   }
 
   public static void para(String text, int w) {
